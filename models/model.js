358d6ae3ddff88a375bbf5a843d816336ffafb78
harsh saxena <kinshuk26harsh@gmail.com>
2022-12-31 14:19:54 +0530

    user login and user updation done

--- models/model.js
+++ models/model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -9,6 +11,13 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Others']
+    },
+    DateOfBirth: {
+        type: Number
     },
     profilepic: {
         type: String,
@@ -21,11 +30,39 @@ const UserSchema = new mongoose.Schema({
     },
     isAdmin: {
         type: Boolean,
-        default: false, 
+        default: false,
     },
+    bio: {
+        type: String,
+        default:null
+    },
+    city: {
+        type: String,
+        default:null
+    },
+    from: {
+        type: String,
+        default : null
+    },
+    relationship: {
+        type: String,
+        enum: ['Single', 'Couple', 'Complicated'],
+        default:'Single'
+    }
 },
     { timestamps: true }
 
 
 )
+UserSchema.pre('save', async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(5);
+        const hashedPassword = await bcrypt.hash(this.password, salt)
+        this.password = hashedPassword
+        next()
+    }
+    catch (error) {
+        next(error)
+    }
+})
 module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,93 @@
 const router = require("express").Router();
 const User = require("../models/model")
 const nodemailer = require("nodemailer")
-const utility = require("../mailer");
+const utility = require("../mailer")
+const bcrypt = require("bcrypt")
 
 
-router.post("/register" , async(req ,res)=>{
-  const newUser = new User({
-    username:req.body.username,
-   email:req.body.email,
-   password:req.body.password
-  });
-  try{
-    const User = await newUser.save();
-    var email=req.body.email
-    let subject = "Register Info";
+
+router.post("/register", async (req, res) => {
+    //   const newUser = new User({
+    //     username:req.body.username,
+    //    email:req.body.email,
+    //    password:req.body.password
+
+    // });
+
+
+    //     var email=req.body.email
+    //     User.findOne({ email: email }).exec((err, user) => {
+    //         console.log('user', user)
+    //         if (user) {
+    //             res.status(400);
+    //              res.send({ error: "user already exists" });
+    //         }
+    //     })
+    //     const User = await newUser.save();
+    //     var email=req.body.email
+    //     let subject = "Register Info";
+    //         let sentTo = email;
+    //         let content = "<b>Thank You for signing Up on Insta-1.0</b>"
+
+    //         let sentResponse = utility.mailsend(subject, sentTo, content);
+
+    //         console.log("Email Send response " + sentResponse);
+    //     res.send("new user registered")
+
+    console.log(req.body);
+    const { username, email, password ,gender ,DateOfBirth } = req.body;
+    console.log('req.body', req.body);
+
+    console.log('email', email)
+
+    User.findOne({ email: email }).exec((err, user) => {
+        console.log('user', user)
+        if (user) {
+            res.status(400);
+            return res.send({ error: "user already exists" });
+        }
+
+        let subject = "Signup Info";
         let sentTo = email;
         let content = "<b>Thank You for signing Up on Insta-1.0</b>"
 
         let sentResponse = utility.mailsend(subject, sentTo, content);
 
         console.log("Email Send response " + sentResponse);
-    res.send("new user registered")
+        let newUser = new User({ username, email, password,gender ,DateOfBirth });
+        console.log('newUser ', newUser);
+        newUser.isNew = true;
+        newUser.save((err, success) => {
+            if (err) {
+                console.log("error on sign:", err);
 
-  }
-  catch{
-    console.log(err)
-  }
+            }
+            res.json({
+                message: "signup is successful"
+            })
+        })
+    })
+
 })
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+
+        res.status(200).json(user)
+        var email = req.body.email
+        let subject = "Login Alert";
+        let sentTo = email;
+        let content = "<b>There is an attempt of login on Insta-1.0 from your Id</b>"
+
+        let sentResponse = utility.mailsend(subject, sentTo, content);
+
+        console.log("Email Send response " + sentResponse);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 module.exports = router
